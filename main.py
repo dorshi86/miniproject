dd2bce7677c4414d8ee3b4cc2effdd7d781d4043
Dor Shiloni <dor@home.shiloni.com>
2020-03-23 23:01:03 +0200

    Done

--- main.py
+++ main.py
@@ -1,47 +1,113 @@
-#from user import User
+import tkinter
 from tkinter import *
-from tkinter.ttk import Combobox, Checkbutton, Radiobutton, Button
+from tkinter import messagebox
+from tkinter.ttk import Combobox
+from user import User
+
+# Main class for the program UI
+class MainWindow:
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+        self.button1 = Button(self.frame, text='Register', width=25, command=self.open_register)
+        self.button1.pack()
+        self.button2 = Button(self.frame, text='Login', width=25, command=self.open_login)
+        self.button2.pack()
+        self.frame.pack()
+
+    def open_register(self):
+        self.newWindow = Toplevel(self.master)
+        self.app = Register(self.newWindow)
+
+    def open_login(self):
+        self.newWindow = Toplevel(self.master)
+        self.app = Login(self.newWindow)
+
+# Class for the register process
+class Register:
+    # Register window
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+
+        self.user_firstname_label = Label(self.master, text="First Name:")
+        self.user_firstname_label.pack()
+
+        self.user_firstname_text_box = Entry(self.master, bd=1)
+        self.user_firstname_text_box.pack()
+
+        self.user_lastname_label = Label(self.master, text="Last Name:")
+        self.user_lastname_label.pack()
+
+        self.user_lastname_text_box = Entry(self.master, bd=1)
+        self.user_lastname_text_box.pack()
+
+        self.user_email_label = Label(self.master, text="Email:")
+        self.user_email_label.pack()
+
+        self.user_email_text_box = Entry(self.master, bd=1)
+        self.user_email_text_box.pack()
+
+        self.user_gender_label = Label(self.master, text="Gender:")
+        self.user_gender_label.pack()
+
+        self.user_gender_combo = Combobox(self.master)
+        self.user_gender_combo['values'] = ('Male','Female')
+        self.user_gender_combo.pack()
+
+        self.user_id_label = Label(self.master, text="User ID:")
+        self.user_id_label.pack()
+
+        self.user_id_text_box = Entry(self.master, bd=1)
+        self.user_id_text_box.pack()
+
+        self.user_pass_label = Label(self.master, text="User Password:")
+        self.user_pass_label.pack()
+
+        self.user_pass_text_box = Entry(self.master, bd=1)
+        self.user_pass_text_box.pack()
+
+        self.enter_button = Button(self.master, text="Enter", command=self.submit)
+        self.enter_button.pack()
+    # Submit process
+    def submit(self):
+        user_firstname = self.user_firstname_text_box.get()
+        user_lastname = self.user_lastname_text_box.get()
+        user_email = self.user_email_text_box.get()
+        user_gender = self.user_gender_combo.get()
+        user_id = self.user_id_text_box.get()
+        user_pass = self.user_pass_text_box.get()
+        Submit=User(user_firstname, user_lastname, user_email, user_gender, user_id, user_pass)
+        messagebox.showinfo(title=Register, message=Submit.create_user())
+
+# Class for the Login process
+class Login:
+    # Login Window
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+
+        self.user_id_label = Label(self.master, text="User ID:")
+        self.user_id_label.pack()
+
+        self.user_id_text_box = Entry(self.master, bd=1)
+        self.user_id_text_box.pack()
+
+        self.user_pass_label = Label(self.master, text="User Password:")
+        self.user_pass_label.pack()
+
+        self.user_pass_text_box = Entry(self.master, bd=1)
+        self.user_pass_text_box.pack()
+
+        self.enter_button = Button(self.master, text="Enter", command=self.submit)
+        self.enter_button.pack()
+    # Submit process
+    def submit(self):
+        user_id = self.user_id_text_box.get()
+        user_pass = self.user_pass_text_box.get()
+        Submit=User(0,0,0,0,user_id,user_pass)
+        messagebox.showinfo(title=Login, message=Submit.validate_user())
 
 root = Tk()
-
-# root.geometry('480x150')
-root.resizable(width=False, height=False)
-root.title('Mini Project')
-
-can1=Canvas(root, width=500, height=500, bg="blue")
-can1.pack(expand=False, fill=X)
-
-lbl1 = Label(can1, text="Dor Shiloni", bg='blue' , fg='white', font=('Arial Bold', 24))
-lbl1.grid(column=1, row=0)
-lbl2 = Label(can1, text="INT Python Course", bg='blue', fg='white', font=('Arial Bold', 24))
-lbl2.grid(column=1, row=1)
-
-b1 = Button(can1, text="Register")
-b1.grid(column=0,row=3)
-
-b1 = Button(can1, text="Login")
-b1.grid(column=3,row=3)
-
-
-
-#
-# combo = Combobox(window)
-# combo['values'] = (1,2,3,4,5,6, 'Text')
-#
-# combo.current(0)
-# combo.grid(column=1, row=1)
-#
-# chk = Checkbutton(window, text='check')
-# chk.grid(column=1, row=2)
-#
-# rad1 = Radiobutton(window, text='first', value=1)
-# rad2 = Radiobutton(window, text='second', value=2)
-# rad3 = Radiobutton(window, text='third', value=4)
-#
-# rad1.grid(column=0, row=3)
-# rad2.grid(column=0, row=3)
-# rad3.grid(column=0, row=3)
-
-# myVar = combo.get()
-
+app = MainWindow(root)
 root.mainloop()
--- user.py
+++ user.py
@@ -2,7 +2,9 @@ import csv
 import hashlib
 import os.path
 
+# This class handles all relevant actions for the User
 class User:
+    # Defining a user
     def __init__(self, user_firstname, user_lastname, user_email, user_gender, user_id, user_pass):
         self.__user_firstname = user_firstname
         self.__user_lastname = user_lastname
@@ -11,9 +13,10 @@ class User:
         self.__user_id = user_id
         self.__user_pass = user_pass
 
+    # Creating a user
     def create_user(self):
         if self.check_if_user_exists(self.__user_id):
-            return 'User Exists'
+            return 'User Exists' # User is being informed if user already exists
         else:
             hashedpass = hashlib.sha256(self.__user_pass.encode()).hexdigest() #Encrypt pass
             if os.path.isfile('user.csv'): # Check if the file exists, if yes it will append, otherwise create a new one
@@ -25,9 +28,11 @@ class User:
                     userscsv = csv.writer(file)
                     userscsv.writerow(["First_Name", "Last_Name", "Email", "Gender", "Username", "Password"])
                     userscsv.writerow([self.__user_firstname, self.__user_lastname, self.__user_email, self.__user_gender, self.__user_id, hashedpass])
+            return f'Welcome {self.__user_firstname}' # If this is a valid registration we welcome the user
 
+    # Checks if a user exists (used by create_user)
     def check_if_user_exists(self, user):
-        if os.path.isfile('user.csv'):  # Check if the file exists
+        if os.path.isfile('user.csv'):  # We start by checking the user.csv exists
             with open('user.csv', newline='') as file:
                 usersdict = csv.DictReader(file)
                 for row in usersdict:
@@ -35,26 +40,19 @@ class User:
         else:
            pass
 
-    def login(self):
-        pass
-
-    def validate_user(self, user, password):
-        if os.path.isfile('user.csv'):  # Check if the file exists
+    # User Login
+    def validate_user(self):
+        if os.path.isfile('user.csv'): # We start by checking the user.csv exists
             with open('user.csv', newline='') as file:
                 usersdict = csv.DictReader(file)
                 for row in usersdict:
-                    if user in row['Username']:
-                        password = hashlib.sha256(password.encode()).hexdigest()
-                        return password in row['Password']
+                    if self.__user_id in row['Username']:
+                        password = hashlib.sha256(self.__user_pass.encode()).hexdigest()
+                        if password in row['Password']:
+                            return f'Welcome {self.__user_id}'
+                        else:
+                            return f'Password is incorrect'
                     else:
                         return 'User not found'
         else:
-           pass
-
-    def validate_email(self):
-        pass
-
-Dor=User('Dor','Shiloni','dorshi@gmail.com','Male','dorshi','2840')
-Dor.create_user()
-#User.check_if_user_exists(1,'dorshsi')
-print(User.validate_user(1,'dorfshi','2840'))
+           return 'User database is missing, cannot process login.'
